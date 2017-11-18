98e9f088ada686c49eb192810826c54d6c51afed
Jane Keibler <wanderingelf14@gmail.com>
2017-11-18 18:24:31 -0500

    Made looping glider Gif
    split cell index from pixel size to make each cell more visible

--- app/Main.hs
+++ app/Main.hs
@@ -9,21 +9,27 @@ import Codec.Picture.Gif
 
 main :: IO ()
 main = either print id $ boardOrError
-  where boardOrError = (mkBoard imageSize imageSize (glider 32 32)) >>= printBoard
+  where boardOrError = (mkBoard boardSize boardSize (glider 0 0)) >>= printBoard
 
 printBoard :: Board -> Either String (IO ())
 printBoard board = writeGifImages "/tmp/glider_animated.gif" LoopingForever gifFrames
   where frames = take maxFrames $ runBoard board
-        pixelFn cells x y = if S.member (x, y) cells then 0 else 255
+        pixelFn cells x y = if S.member (x `quot` cellSize, y `quot` cellSize) cells then 0 else 255
         cellsToImage cells = generateImage (pixelFn cells) imageSize imageSize
         images = cellsToImage <$> frames
         gifFrames = (\x -> (greyPalette, 10, x)) <$> images
 
+boardSize :: Int
+boardSize = 16
+
+cellSize :: Int
+cellSize = 16
+
 imageSize :: Int
-imageSize = 64
+imageSize = boardSize * cellSize
 
 maxFrames :: Int
-maxFrames = 60
+maxFrames = 64
 
 glider :: Int -> Int -> CellSet
 glider offX offY = S.fromList $ shiftCell <$> [(1,0), (2,1), (0,2), (1,2), (2,2)]
